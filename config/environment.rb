3935a795b98a4f6b356700d915c00b31fe37b422
nsanya <nsanya@inbox.ru>
2012-04-23 10:54:20 +0300

    newGit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Glava2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glava2::Application.config.secret_token = '860ec9073cd4906eee5b0112747b84117fd95e662e903478a4ae6af3587b401fbe657193bacd0189a7b2670c066e9ab5359696208065221d5155245af67a195d'
